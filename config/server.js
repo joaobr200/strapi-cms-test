dd6a4aa6862e29bc7455861f9ae57c450b613cc4
Joaobr200 <joaoarantesjob@gmail.com>
2020-09-28 17:56:27 -0300

    inital commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '221f33404801da8668878a56be3a2803'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd28b2864-548f-4169-af37-c3cdc1fd9de3'
+};
